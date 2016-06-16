70f77814377a09353332133353d5dc7430361e14
khachiai oussama <oussama.khachiai@gmail.com>
2016-06-16 15:06:21 +0000

    adding sonata admin bundle

--- src/Usmba/FLRegistrationBundle/Form/Type/EtudiantType.php
+++ src/Usmba/FLRegistrationBundle/Form/Type/EtudiantType.php
@@ -2,15 +2,15 @@
 
 namespace Usmba\FLRegistrationBundle\Form\Type;
 
+use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\BirthdayType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
-use Symfony\Component\Form\Extension\Core\Type\RadioType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Usmba\FLRegistrationBundle\Entity\TypeBac;
 
 class EtudiantType extends AbstractType
 {
@@ -22,20 +22,16 @@ class EtudiantType extends AbstractType
     {
         $builder
             ->add('cne', TextType::class)
-            ->add('nom', TextType::class)
-            ->add('prenom', TextType::class)
+            ->add('nomfr', TextType::class)
+            ->add('prenomfr', TextType::class)
             ->add('nomAr', TextType::class)
             ->add('prenomAr', TextType::class)
-            ->add('sexe', RadioType::class)
-            ->add('handicap', RadioType::class)
+            ->add('sexe', TextType::class)
+            ->add('handicap', TextType::class)
             ->add('dateNaiss', BirthdayType::class)
             ->add('lieuNaissFr', TextType::class)
             ->add('lieuNaissAr', TextType::class)
             ->add('cin', TextType::class)
-            ->add('situationFamille', SituationFamilleType::class)
-            ->add('categorieSocioProfessionnelle', CategorieSocioProfessionnelleType::class)
-            //Coordonnés
-            ->add('dateObtBac', DateType::class)
             ->add('adresseEtd', TextareaType::class)
             ->add('gsm', TextType::class)
             ->add('email', EmailType::class)
@@ -45,12 +41,15 @@ class EtudiantType extends AbstractType
             ->add('nomPrenomMereFr', TextType::class)
             ->add('nomPrenomMereAr', TextType::class)
             ->add('fontionMere', TextType::class)
-            //Informations Baccalauéat
-            ->add('typeBac', TextType::class)
+            ->add('dateObtBac', DateType::class)
+            ->add('ville', VilleType::class)
+            ->add('typeBac', TypeBacType::class)
             ->add('academie', AcademieType::class)
             ->add('serieDuBac', SerieDuBacType::class)
             ->add('mentionBac', MentionType::class)
-            ->add('dateObtBac', DateType::class)
+            ->add('etablissementBac', EtablissementBacType::class)
+            ->add('situationFamille', SituationFamilleType::class)
+            ->add('categorieSocioProfessionnelle', CategorieSocioProfessionnelleType::class)
         ;
     }
     
