501cf030689ca53351590750751d666f44ba6d47
KHACHIAI Oussama <oussama.khachiai@gmail.com>
2016-06-21 01:16:02 +0200

    costomise the admin layout

--- src/ReregistrationBundle/Controller/ReInscriptionController.php
+++ src/ReregistrationBundle/Controller/ReInscriptionController.php
@@ -2,6 +2,7 @@
 
 namespace ReregistrationBundle\Controller;
 
+use ReregistrationBundle\Entity\Etudiant;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
@@ -22,5 +23,34 @@ class ReInscriptionController extends Controller
         return $this->render('Inscription/formReInscription.html.twig', [
 
         ]);
+    }
+
+    /**
+     * Displays a form to edit an existing Etudiant entity.
+     *
+     */
+    public function editAction(Request $request, Etudiant $etudiant)
+    {
+        $cne = $request->request->get('cne');
+        $em = $this->getDoctrine()->getManager();
+        $etudiant = $em->getRepository('ReregistrationBundle:Etudiant')->findOneBy([
+           'cne' => $cne 
+        ]);
+
+        $editForm = $this->createForm('ReregistrationBundle\Form\EtudiantType', $etudiant);
+        $editForm->handleRequest($request);
+
+        if ($editForm->isSubmitted() && $editForm->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($etudiant);
+            $em->flush();
+
+            return $this->redirectToRoute('etudiant_edit', array('id' => $etudiant->getId()));
+        }
+
+        return $this->render(':Inscription:reInscriptionForm.html.twig', array(
+            'etudiant' => $etudiant,
+            'form' => $editForm->createView(),
+        ));
     }
 }
